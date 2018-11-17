511afa510c2f41e5749554a707eaa26a37818168
katiedonkers <36115268+katiedonkers@users.noreply.github.com>
2018-11-17 18:12:05 -0500

    headless pi sends measurements to android
    updated

--- Headless_Pi_SendToAndroid.py
+++ Headless_Pi_SendToAndroid.py
@@ -1,6 +1,7 @@
 import socket, sys, time
 
-measurementsPacketFlag = 5 
+newMeasurementsPacketFlag = 3
+temp = 99
 
 # function to collect and send measurements from headless Pi to database Pi
 def sendMeasurements(data):
@@ -8,17 +9,27 @@ def sendMeasurements(data):
     host = '192.168.43.231'
     port = 1050
     server_address = (host, port)
+    host_address = ('192.168.43.128', 53477)
 
     #create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print('trying to bind')
+    s.bind(host_address)
+    print('bound')
 
     # send packet to database Pi
     s.sendto(data, server_address)
+    print('sent packet')
+
+    #time.sleep(10)
+    print('waiting to receive...')
+    buf, address = s.recvfrom(2048)
+    print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
 
 def formatPacket(data):
 
-    #format packet
-    temp = "" + str(measurementsPacketFlag) + "" + str(data[0]) + "" + str(data[1]) + "" + str(data[2]) + ""
+    # format packet
+    temp = "" + str(newMeasurementsPacketFlag) + "" + str(data[0]) + "" + str(data[1]) + "" + str(data[2])
     byteArray = bytearray(temp, 'utf8')
 
     # call sendMeasurements() with proper packet format
@@ -27,22 +38,12 @@ def formatPacket(data):
 #MAIN
 def main():
 
-    count = 1
-#measurements array [] will contain length, circumf1 (wrist) circumf2 (elbow) 
-    measurements = [1,2,3]
-
-    while (len(measurements) < 3):
-        count+=1
-   
+    #measurements array [] will contain length, circumf1 (wrist) circumf2 (elbow) 
+    measurements = [7,8,9]
     formatPacket(measurements)
-    print('hi')
 
 
 main()
-#print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
-#s.shutdown(1)
-
-
 
 
 
